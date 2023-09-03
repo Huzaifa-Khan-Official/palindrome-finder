1008422b1ab1f99f243cf1402907d70239317d14
Huzaifa Khan <122217933+Huzaifa-Khan0@users.noreply.github.com>
2023-09-03 19:23:36 +0500

    palindrome updated

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var resultPara = document.querySelector("#result");
 var resultDiv = document.querySelector(".resultDiv");
 
 function findPalindrome() {
-    var usserInputArray = userInput.value.split("");
+    var userInputLowerCase = userInput.value.toLowerCase();
+    var usserInputArray = userInputLowerCase.split("");
     var reversedArray = [];
-    for(var i = usserInputArray.length - 1; i >= 0; i--) {
+    for (var i = usserInputArray.length - 1; i >= 0; i--) {
         reversedArray.push(usserInputArray[i]);
     }
     var reversed = reversedArray.join('');
     resultDiv.classList.remove("d-none")
-    resultDiv.style.display  = "block";
+    resultDiv.style.display = "block";
     reversedDiv.value = reversed;
 
-    if (userInput.value == reversed) {
+    if (userInputLowerCase == reversed) {
         resultPara.innerHTML = "It is palindrome";
     } else {
         resultPara.innerHTML = "It is not a palindrome";
